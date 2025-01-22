7507e8b483ace2431afc7d6c9687b87573cba971
gabriel jimenez <gabo0813ajm@gmail.com>
2025-01-22 12:41:48 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -28,5 +28,62 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class Identity(Base):  
+    __tablename__ = 'identity'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    descrption = Column(String(250))
+    
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    eye_color = Column(String(250))
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    mass = Column(String(250))
+    heigth = Column(String(250))
+    gender = Column(String(250))
+    birth_year = Column(String(250))
+    identity_id = Column(Integer, ForeignKey('identity.id'))
+    identity = relationship(Identity)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    climate = Column(String(250))
+    diameter = Column(String(250))
+    gravity = Column(String(250))
+    population = Column(String(250))
+    terrain = Column(String(250))
+    orbital_period = Column(String(250))
+    rotation_period = Column(String(250))
+    identity_id = Column(Integer, ForeignKey('identity.id'))
+    identity = relationship(Identity)
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250))
+    length = Column(String(250))
+    passengers= Column(String(250))
+    manufacturer = Column(String(250))
+    vehicle_class = Column(String(250))
+    cargo_capacity = Column(String(250))
+    crew = Column(String(250))
+    cost_in_credits = Column(String(250))
+    identity_id = Column(Integer, ForeignKey('identity.id'))
+    identity = relationship(Identity)
+
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    identity_id = Column(Integer, ForeignKey('identity.id'))
+    identity = relationship(Identity)
+
+    
+   
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
